238b4003ca42d6eb38eb26bebcf9391b66302fa8
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-19 15:24:37 +0200

    updates comments, logs and README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # moodle-scrapper
-Downloading all the content from Moodle course page is tremendously long and boring. This script aims to automate the download of a Moodle page's available material
+
+Downloading all the content from Moodle course page is tremendously long and boring. This script aims to automate the download of a Moodle page's available material.
+
+## History
+
+In order to archive material and be able to efficiently work for exams, it's useful to get all documents. Moodle has "protections" to prevent batch downloads. This scripts aims to be a workaround the workaround :)
+
+## Usage
+
+### Avoid opening all in tabs
+
+First, make sure that you do not have automatic opening of PDF files (this will avoid having 35+ open tabs by the script). For example in Firefox: ![change_pdf_download_settings](readme-assets/change_pdf_download_settings.png).
+
+### Where this script can be used
+
+You can use the script once in a course's page. For example, you can go to the AICC-II course page via [https://moodle.epfl.ch/course/view.php?id=15838](https://moodle.epfl.ch/course/view.php?id=15838).
+
+### Where to put the script
+
+The script uses JavaScript and should be run in the interactive console of your web browser. The choice for this is that using directly you browser remove the need for dealing with Tequila: you are already using the environment you are logged in. For example to open the console in Firefox, you can do:
+
+![console_location_1](readme-assets/console_location_1.png)
+
+![console_location_2](readme-assets/console_location_2.png)
+
+In the open console, you can paste the script and click on "execute". You will be prompted for the download of each files (you can select "apply same rule to all" and download everything at once) 🥳.
+
+### What this script does
+
+This script goes through all resources present on the main page and download these, bypassing the Moodle protection. This could be done by hand and is not forbidden, it's just faster. However be aware that batch sharing the material on public platforms follows other laws, make sure not to break these ;)
+
+This script only downloads PDF files (slides and lecture notes in general). For convenience, we did not include other types of media as these could be large (mp4 video) or redundant (pptx of the PDF).
+
+### What this script does not
+
+This script does not download files of type other than PDF nor does it download file present in "folder" or forums.
--- moodle-scrapper.js
+++ moodle-scrapper.js
@@ -1,14 +1,18 @@
 // https://stackoverflow.com/questions/54626186/how-to-download-file-with-javascript
 // https://www.aspsnippets.com/Articles/JavaScript-Save-BLOB-as-PDF-File.aspx
 function callback_get_pdf(xmlHTTP, filename) {
+  // to be called with an xmlRequest for a desired file
   var link = document.createElement("a");
   uri_split = xmlHTTP.responseURL.split(".");
   extension = "." + uri_split[uri_split.length - 1]
   link.download = filename + extension;
   if (extension !== ".pdf") {
-    console.log("non pdf file")
-    return
+    // we currently don't allow other downloads than pdf
+    // be very careful, downloading mp4 can be very long for example
+    return;
   }
+  // we must create a new blob, that's a thing for pdf downloads askip
+  // https://attacomsian.com/blog/javascript-download-file
   var blob = xmlHTTP.response;
   blob = new Blob([xmlHTTP.response], {
     type: "application/octetstream"
@@ -16,15 +20,17 @@ function callback_get_pdf(xmlHTTP, filename) {
   link.href = URL.createObjectURL(blob);
   link.target = "_blank"
   link.click();
-  link.remove()
+  // we remove the elements to avoid explosion
+  link.remove();
 }
 
 function callback_get_last_link(xmlHttp, filename) {
-  console.log("Document queried successfully");
+  // Takes care of dodging moodle workaround that puts the real link in another html file
   resp = xmlHttp.response;
   elem = resp.getElementsByClassName("resourceworkaround");
   for (resource in elem) {
     if (elem[resource].childElementCount > 0) {
+      // we obtain the real link to the resource and get it as a blob
       last_link = elem[resource].firstElementChild.href;
       httpGetAsync(last_link, "blob", callback_get_pdf, filename)
     }
@@ -32,12 +38,15 @@ function callback_get_last_link(xmlHttp, filename) {
 }
 
 // https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+// https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest
 function httpGetAsync(theUrl, response_type, callback, filename) {
   var xmlHttp = new XMLHttpRequest();
+  // not required for me but in case, makes sure we forward cookies for tequila login
   xmlHttp.withCredentials = true
   xmlHttp.responseType = response_type;
-  xmlHttp.onreadystatechange = function() {
+  xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState === 4 && xmlHttp.status == 200) {
+      // we call the callback only when request is done and successful
       callback(xmlHttp, filename)
     }
   }
@@ -50,6 +59,7 @@ links = document.getElementsByClassName("aalink")
 test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for (const link_id in links) {
   if (links[link_id].href != undefined && links[link_id].href.includes("moodle.epfl.ch/mod/resource")) {
+    // we make sure to query only resources (not forums or quizzes)
     httpGetAsync(links[link_id].href, "document", callback_get_last_link, links[link_id].text);
   }
 }
